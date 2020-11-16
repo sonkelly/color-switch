ddaae818ef2cf5a5c6d4c413cafafe64d30b7454
Sơn Smoker <43397064+sonkelly@users.noreply.github.com>
2020-11-16 22:42:33 +0700

    Add files via upload

---
+++ Assets/scripts/main_controller.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class main_controller : MonoBehaviour
+{
+    public GameObject circle_main, circle_main_2, circle_main_3;
+    public float rotSpeed = 5f;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        circle_main.transform.Rotate(new Vector3(0, 0, 1 * rotSpeed));
+        circle_main_2.transform.Rotate(new Vector3(0, 0, -1 * rotSpeed));
+        circle_main_3.transform.Rotate(new Vector3(0, 0, 1 * rotSpeed));
+    }
+    public void startGame() {
+        SceneManager.LoadScene(1);
+    }
+}
---
+++ Assets/scripts/player_controller.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class player_controller : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public float speed = 600f;
+    private Rigidbody2D player;
+    void Start()
+    {
+        player = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (Input.GetMouseButtonDown (0)) {
+            if (player != null) {
+                player.AddForce(Vector2.up * speed);
+            }
+        }
+
+
+        if (Input.GetMouseButtonDown (1)) {
+        }
+
+
+        if (Input.GetMouseButtonDown (2)) {
+        }
+
+    }
+}
